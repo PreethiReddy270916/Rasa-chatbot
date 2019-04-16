d6dc09759fa2dd9dea6ccf6f435e2f1b3769da35
Preethi <preethireddy27916@gmail.com>
2019-04-15 23:37:31 -0700

    Testing chatbot through a console

--- test_model.py
+++ test_model.py
@@ -2,12 +2,12 @@ from rasa_core.channels.console import ConsoleInputChannel
 from rasa_core.agent import Agent
 from rasa_core.interpreter import RasaNLUInterpreter
 import numpy as np
+
 def food_bot(serve_forever=True):
 	interpreter =RasaNLUInterpreter('./models/nlu/default/foodnlu')
 	agent=Agent.load('./models/dialogue', interpreter=interpreter)
-    	if serve_forever:
+    if serve_forever:#for the console to be running indefinitely 
 		agent.handle_channel(ConsoleInputChannel())
 	return agent
-
 
 food_bot()
